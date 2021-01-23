0c6bb72781ac0defecb35b0f1ad0dd523bdcdc8d
boryabes <box.bern@yandex.ru>
2021-01-23 12:41:33 +0600

    finish

--- sources/main.cpp
+++ sources/main.cpp
@@ -3,17 +3,17 @@
 
 #include "iostream"
 #include "parser.hpp"
-int main(int argc, char *argv[]) { //интаргси количество аргументов, чараргви массив с аргументами
-    std::string path; //создаю строку пас
+int main(int argc, char *argv[]) {
+    std::string path;
 
-    if (argc == 1) { //если количество аргументов 1(просто название скрипта) то устанавливаю путь в текущую директорию
-        path = "."; //. где я нахожусь сейчас(текущая директория)
+    if (argc == 1) {
+        path = ".";
 
     } else {
-        path = argv[1]; //если аргументов больше единицы, то путь это второй аргумент
+        path = argv[1];
     }
 
-    parserOfDir parser(path); //парсю все директории, передаю в аргументы путь к папке которую буду парсить
-    parser.assembleFilesForAcc(); //собираю файлы для аккаунтов
-    std::cout<<parser.printFormatted(); //вывожу
+    parserOfDir parser(path);
+    parser.assembleFilesForAcc();
+    std::cout<<parser.printFormatted();
 }
